ef1a0eadfc41cefaaed0f0710168b51fb43a6b19
Harsh kumar <68609382+harsh6768-svg@users.noreply.github.com>
2021-04-07 21:00:17 +0530

    updates
    Signed-off-by: Harsh kumar <68609382+harsh6768-svg@users.noreply.github.com>

---
+++ hii.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	  #ifndef ONLINE_JUDGE 
+  
+    // For getting input from input.txt file 
+    // For getting input from input.txt file 
+    freopen("input.exe", "r", stdin); 
+  
+    // Printing the Output to output.txt file 
+    freopen("output.exe", "w", stdout); 
+  
+#endif
+    int n;
+    cin>>n;
+    cout<<n<<endl;
+}
